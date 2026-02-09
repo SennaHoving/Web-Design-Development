782f3538847a6724fa2e612513d35d103cfba8bd
SennaHoving <senna.hoving@gmail.com>
2026-02-09 11:40:33 +0100

    fetch data

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+async function fetchData() {
+    try {
+        const response = await fetch('https://fdnd.directus.app/items/person/332');
+        const data = await response.json();
+        if(data) {
+            selectElements(data);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+fetchData();
+
+selectElements = (data) => {
+    Object.entries(data.data).forEach(([key, value]) => {
+        const element = document.querySelector(`[data="${key}"]`);
+        if (element) {
+            element.textContent = value;
+        }
+    });   
+}
